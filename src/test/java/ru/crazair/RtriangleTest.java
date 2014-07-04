dd96af2959d4c8ea26609377f3cb9f569b397ffe
anosv <anosv@anosv>
2014-07-04 13:21:47 +0400

    remote excess

--- src/test/java/ru/crazair/RtriangleTest.java
+++ src/test/java/ru/crazair/RtriangleTest.java
@@ -2,7 +2,7 @@ package ru.crazair;
 
 import static org.junit.Assert.*;
 import static java.lang.Math.*;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import ru.crazair.rjt.RtriangleProvider;
 import ru.crazair.rjt.Triangle;
@@ -12,10 +12,10 @@ import ru.crazair.rjt.Triangle;
  */
 public class RtriangleTest {
 
-    Triangle triangle;
+    private static Triangle triangle;
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUp() {
         triangle = (Triangle) RtriangleProvider.getRtriangle();
     }
 
@@ -25,13 +25,8 @@ public class RtriangleTest {
         double secondSideLength = triangle.getSortedSides()[1];
         double thirdSideLength  = triangle.getSortedSides()[2]; // hypotenuse
 
-        assertFalse("Triangle is not correct, all side length equals 0: " + triangle,
-                firstSideLength == 0 && secondSideLength == 0 && thirdSideLength == 0);
-        
-        assertFalse("Duplicates the vertex coordinates of the triangle: " + triangle, 
-                ((triangle.getApexX1() == triangle.getApexX2()) && (triangle.getApexY1() == triangle.getApexY2())) ||
-                ((triangle.getApexX2() == triangle.getApexX3()) && (triangle.getApexY2() == triangle.getApexY3())) ||
-                ((triangle.getApexX3() == triangle.getApexX1()) && (triangle.getApexY3() == triangle.getApexY1())));
+        assertFalse("Triangle is not correct, side length equals 0: " + triangle,
+                firstSideLength == 0 || secondSideLength == 0 || thirdSideLength == 0);
 
         assertTrue("Triangle is not right, Pythagoras in sorrow: " + triangle,
                 round(pow(firstSideLength, 2) + pow(secondSideLength, 2)) == round(pow(thirdSideLength, 2)));
